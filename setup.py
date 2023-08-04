282104f258d77a70d4879bbed6cba0aebafa73fe
Beetoo Goswami <beetoo.goswami85@gmail.com>
2023-08-03 22:44:49 -1200

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __version__ = "0.0.0"
 
 REPO_NAME="Text-Summarizer-Project"
 AUTHOR_USER_NAME="Beetoo"
-SRC_REPO="text-summarizer"
+SRC_REPO="textSummarizer"
 AUTHOR_EMAIL="beetoo.goswami@gmail.com"
 
 setuptools.setup(
